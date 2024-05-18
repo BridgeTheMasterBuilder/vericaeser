8730e8592956d1cc292dc15a0ffba026387e3d8b
Gylfi Guðjohnsen <gylfi@stebbi.is>
2024-05-18 22:50:30 +0000

    Created an induction principle for text, cleaned up proofs

--- theories/caesar.v
+++ theories/caesar.v
@@ -65,7 +65,24 @@ Definition ascii_of_Z (n : Z) := ascii_of_nat (Z.to_nat n).
 
 Definition is_uppercase_encoded (c : ascii) : Prop := 0 <= Z_of_ascii c < alphabet_size.
 
-Definition text := { s : string | Forall is_uppercase_encoded (list_ascii_of_string s) }.
+Definition string_is_uppercase_encoded (s : string) := Forall is_uppercase_encoded (list_ascii_of_string s).
+
+Definition text := { s : string | string_is_uppercase_encoded s }.
+
+Open Scope string_scope.
+
+Definition text_ind : forall P : text -> Prop,
+       (forall (H : string_is_uppercase_encoded ""), P (exist _ "" H)) ->
+       (forall (c : ascii)
+          (s : string)
+          (H1 : string_is_uppercase_encoded s)
+          (H2 : string_is_uppercase_encoded (String c s)),
+           P (exist _ s H1) -> P (exist _ (String c s) H2)) ->
+       forall t : text, P t.
+  move => P base step.
+  case; elim => //=; unfold string_is_uppercase_encoded.
+  by move => c ? ? H; inversion H as [|? ? ? H']; apply (step _ _ H' _).
+Qed.
 
 Definition encode (c : ascii) : ascii :=
   ascii_of_Z (Z_of_ascii c - ascii_A).
@@ -73,14 +90,24 @@ Definition encode (c : ascii) : ascii :=
 Definition decode (c : ascii) : ascii :=
   ascii_of_Z (Z_of_ascii c + ascii_A).
 
+Lemma encode_correct : forall (c : ascii), is_ascii_uppercase c -> is_uppercase_encoded (encode c).
+Proof.
+  move => [[] [] [] [] [] [] [] []]; easy.
+Qed.
+
+Lemma decode_correct : forall (c : ascii), is_uppercase_encoded c -> is_ascii_uppercase (decode c).
+Proof.
+  move => [[] [] [] [] [] [] [] []] []; easy.
+Qed.
+
 Definition shifted k c := { c' : ascii | Z_of_ascii c' = (Z_of_ascii c + k) mod alphabet_size }.
 
-Hint Unfold alphabet_size ascii_A thirteen is_ascii_uppercase is_uppercase_encoded proj1_sig Z_of_ascii ascii_of_Z encode decode : caesar_db.
+Hint Unfold alphabet_size ascii_A thirteen is_ascii_uppercase is_uppercase_encoded proj1_sig Z_of_ascii ascii_of_Z encode decode string_is_uppercase_encoded : caesar_db.
 
 Program Definition shift (k : Z) (c : ascii) : shifted k c :=
   ascii_of_Z ((((Z_of_ascii c + k) mod alphabet_size) + alphabet_size) mod alphabet_size).
 Next Obligation.
-  autounfold with caesar_db; move => * //=; rewrite nat_ascii_embedding; lia.
+  autounfold with caesar_db; move => *; rewrite nat_ascii_embedding; lia.
 Qed.
 
 Program Definition text_of_string (s : string) : option text :=
@@ -89,20 +116,18 @@ Program Definition text_of_string (s : string) : option text :=
     | false => None
   end.
 Next Obligation.
+  unfold string_is_uppercase_encoded.
   elim => //=.
-  move => c s' IHs'.
-  move => /(@eq_sym bool) /andP [H1 H2].
-  autounfold with caesar_db.
-  destruct c; repeat match goal with [ b : bool |- _ ] => destruct b end; inversion H1; (constructor; [rewrite nat_ascii_embedding //=; lia |]); auto.
+  move => ? ? IHs' /(@eq_sym bool) /andP [? ?].
+  by constructor; [apply encode_correct | by auto].
 Qed.
 
 Program Definition string_of_text (t : text) : { s : string | String.forallb is_ascii_uppercase s = true } := String.map decode t.
 Next Obligation.
-  move => [t H] //=; move: H.
-  induction t as [| c s' IHs'] => //=.
-  move => H; inversion H as [|? ? [H1 H2] ?]; subst.
-  rewrite andb_true_iff; split; auto.
-  destruct c; repeat match goal with [ b : bool |- _ ] => destruct b end; inversion H2; auto.
+  elim/text_ind => //=.
+  unfold string_is_uppercase_encoded.
+  move => ? ? ? /Forall_cons_iff [? ?] ?.
+  by apply/andP; split; [apply decode_correct |].
 Qed.
 
 Program Definition encrypted (ciphertext : text) (plaintext : text) (k : Z) : Prop :=
@@ -116,19 +141,15 @@ Hint Unfold shift encrypted decrypted : caesar_db.
 Program Definition encrypt (s : text) (k : Z) : { s' : text | encrypted s' s k } :=
   String.map (fun c => shift k c) s.
 Next Obligation.
-  move => [s H] k; autounfold with caesar_db.
-  elim s.
-  - constructor.
-  - move => c s' IHs' //=.
-    constructor; auto.
-    autounfold with caesar_db; rewrite nat_ascii_embedding; lia.
+  elim/text_ind => //=.
+  unfold string_is_uppercase_encoded.
+  move => ? ? ? /Forall_cons_iff [? ?] ? ?.
+  constructor => //=; autounfold with caesar_db; rewrite nat_ascii_embedding; lia.
 Qed.
 Next Obligation.
-  move => [s H] k; autounfold with caesar_db.
-  elim s.
-  - constructor.
-  - move => c s' IHs' //=.
-    constructor; auto.
+  autounfold with caesar_db;
+  elim/text_ind; first by constructor.
+  by constructor.
 Qed.
 
 Program Definition decrypt (s : text) (k : Z) : { s' : text | decrypted s' s k } :=
@@ -138,34 +159,28 @@ Definition rot13 (s : text) : { s' | encrypted s' s thirteen } := encrypt s thir
 
 Lemma encrypt_id : forall (s : text), (` (` (encrypt s 0))) = (` s).
 Proof.
-  move => [s H] //=.
-  induction s as [| c s IHs].
-  - easy.
-  - move => //=; f_equal; inversion H as [| ? ? [H1 H2]]; subst.
-    + destruct c; repeat match goal with [ b : bool |- _ ] => destruct b end; inversion H2; auto.
-    + now apply IHs.
+  elim/text_ind => //=.
+  autounfold with caesar_db.
+  move => c ? ? /Forall_cons_iff [[? ?] ?] ?; f_equal => //=.
+  suff {2} <- : ascii_of_nat (Z.to_nat (Z.of_nat (nat_of_ascii c))) = c by f_equal; lia.
+  by rewrite Nat2Z.id ascii_nat_embedding.
 Qed.
 
 Lemma encrypt_mod : forall (s : text) (k : Z), (` (` (encrypt s k))) = (` (` (encrypt s (k mod alphabet_size)))).
 Proof.
-  move => [s H] k //=.
-  induction s as [| c s IHs].
-  - easy.
-  - simpl; f_equal; inversion H; subst.
-    + f_equal. now rewrite Zplus_mod_idemp_r.
-    + now apply IHs.
+  elim/text_ind => //=.
+  autounfold with caesar_db.
+  move => c ? ? /Forall_cons_iff [[? ?] ?] ? ?; f_equal => //=.
+  by rewrite Zplus_mod_idemp_r.
 Qed.
 
 Lemma encrypt_trans : forall (s : text) (k l : Z),
     (` (` (encrypt (` (encrypt s k)) l))) = (` (` (encrypt s (k + l)))).
 Proof.
-  move => [s H] k l //=.
-  induction s as [| c s IHs].
-  - easy.
-  - simpl; f_equal.
-    + autounfold with caesar_db.
-      rewrite nat_ascii_embedding; [| f_equal ]; lia.
-    + inversion H; now apply IHs.
+  elim/text_ind => //=.
+  autounfold with caesar_db.
+  move => ? ? ? /Forall_cons_iff [[? ?] ?] ? ? ?; f_equal => //=.
+  rewrite nat_ascii_embedding; [| f_equal ]; lia.
 Qed.
 
 Corollary rot13_involutive : forall (s : text), (` (` (rot13 (` (rot13 s))))) = (` s).
